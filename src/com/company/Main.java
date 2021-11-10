ced44a13499de6f53005a3a4519b33f1357cad4b
SoftwareSmeden <torb815g@edu.easj.dk>
2021-11-10 12:19:32 +0100

    Navneændring for parameter i de forskellige metoder

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -35,16 +35,16 @@ public class Main {
     }
 
     //Opg. 3 ---> Indlæs informationerne fra tekstfilen ”bestilling.txt” i VareData-objekter.
-    public static int indlaesFraFil(Vare[] a) throws FileNotFoundException {
+    public static int indlaesFraFil(Vare[] array) throws FileNotFoundException {
 
         File ordre = new File("bestilling.txt");
         Scanner input = new Scanner(ordre);
 
         int i = 0;
         while(input.hasNext()){
-            a[i].setStk(input.nextInt());
-            a[i].setNavn(input.next());
-            a[i].setPris(input.nextDouble());
+            array[i].setStk(input.nextInt());
+            array[i].setNavn(input.next());
+            array[i].setPris(input.nextDouble());
             i++;
         }
         input.close();
@@ -53,33 +53,33 @@ public class Main {
     }
 
     //Opg. 4 ---> Lav en funktion udskriv, som modtager et array af VareData og som udskriver indholdet til skærm.
-    public static void udskrivArr(Vare[] a, int antal){
+    public static void udskrivArr(Vare[] array, int antal){
 
         for (int i = 0; i < antal; i++) {
-            System.out.format("%d %s %.2f \n", a[i].getStk(), a[i].getNavn(),a[i].getPris());
+            System.out.format("%d %s %.2f \n", array[i].getStk(), array[i].getNavn(),array[i].getPris());
         }
     }
 
     //Opg. 5 ---> Skriv indholdet af arrayet med VareData til en datafil “varer.ser” med ObjectOutputStream.
-    public static void skrivTilDatFil(Vare[] a2, int antal) throws IOException {
+    public static void skrivTilDatFil(Vare[] array, int antal) throws IOException {
 
         ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("varer.ser"));
 
         for (int i = 0; i < antal; i++) {
-            out.writeObject(a2[i]);
+            out.writeObject(array[i]);
         }
             out.close();
     }
 
     //Opg. 6 ---> Indlæs fra datafil “varer.ser” med ObjectInputStream til et array af VareData objekter. Kald på funktionen udskriv.
-    public static int indlaesFraSerFil(Vare[] a3) throws IOException, ClassNotFoundException {
+    public static int indlaesFraSerFil(Vare[] array) throws IOException, ClassNotFoundException {
 
         FileInputStream f = new FileInputStream("varer.ser");
         ObjectInputStream in = new ObjectInputStream(f);
 
         int i = 0;
         while(f.available() > 0){
-            a3[i] = (Vare) in.readObject();
+            array[i] = (Vare) in.readObject();
             i++;
         }
         in.close();
@@ -88,21 +88,21 @@ public class Main {
     }
 
     //Opg. 7 ---> Lav funktion som beregner den samlede pris for hver vare med og uden rabat.
-    public static void udskrivSamledePris(Vare[] a4, int antal){
+    public static void udskrivSamledePris(Vare[] array, int antal){
 
         System.out.format("\n%-20s %-20s %-20s %s\n", "Varenavn", "Stk", "Samlet pris", "Samlet pris med rabat");
 
         for (int i = 0; i < antal; i++) {
-            if (a4[i].getStk() > 10) {
-                System.out.format("\n%-20s %-25d %-30.2f %.2f\n",a4[i].getNavn(),a4[i].getStk(),a4[i].getPris() * a4[i].getStk(),(a4[i].getPris() * a4[i].getStk() * 0.85));
+            if (array[i].getStk() > 10) {
+                System.out.format("\n%-20s %-25d %-30.2f %.2f\n",array[i].getNavn(),array[i].getStk(),array[i].getPris() * array[i].getStk(),(array[i].getPris() * array[i].getStk() * 0.85));
             }else {
-                System.out.format("\n%-20s %-25d %-31.2f\n",a4[i].getNavn(),a4[i].getStk(),a4[i].getPris() * a4[i].getStk());
+                System.out.format("\n%-20s %-25d %-31.2f\n",array[i].getNavn(),array[i].getStk(),array[i].getPris() * array[i].getStk());
             }
         }
     }
 
     //Opg. 8 ---> Lav funktion, som beregner saldoen for det samlede varekøb med og uden rabatter.
-    public static void totalSamlet(Vare[] a5, int antal, Vare total){
+    public static void totalSamlet(Vare[] array, int antal, Vare total){
 
         System.out.format("\n%53s %30s\n","Total pris", "Total pris med rabat");
 
@@ -110,10 +110,10 @@ public class Main {
         double prisTotal = 0.0;
 
         for (int i = 0; i < antal; i++) {
-            if (a5[i].getStk() > 10){
-                rabatPrisTotal += (a5[i].getPris() * 0.85) * a5[i].getStk();
+            if (array[i].getStk() > 10){
+                rabatPrisTotal += (array[i].getPris() * 0.85) * array[i].getStk();
             }
-            prisTotal += a5[i].getPris() * a5[i].getStk();
+            prisTotal += array[i].getPris() * array[i].getStk();
         }
 
         total.setTotalPris(prisTotal);
@@ -124,7 +124,7 @@ public class Main {
 
     //Opg. 9 ---> Lav funktion, som udskriver faktura hvor der er informationer om: hvilke varer der er købt, antallet af hver vare, prisen for hver vare med og uden rabat, og den samlede saldo med og uden rabat.
     //Fakturaen skal både udskrives til skærm og til en ny tekstfil ”faktura.txt”.
-    public static void faktura(Vare[] a6, int antal, Vare total) throws IOException {
+    public static void faktura(Vare[] array, int antal, Vare total) throws IOException {
 
         //Opretter file:
         File nyFil = new File("Faktura.txt");
@@ -137,10 +137,10 @@ public class Main {
         System.out.format("\n%-20s %-20s %-20s %-20s %s\n", "Varenavn", "Stk", "Stk-pris", "Samlet pris", "Samlet pris med rabat");
 
         for (int i = 0; i < antal; i++) {
-            if (a6[i].getStk() > 10) {
-                System.out.format("\n%-20s %-23d %-22.2f %-30.2f %.2f\n",a6[i].getNavn(),a6[i].getStk(),a6[i].getPris(),a6[i].getPris() * a6[i].getStk(),(a6[i].getPris() * a6[i].getStk() * 0.85));
+            if (array[i].getStk() > 10) {
+                System.out.format("\n%-20s %-23d %-22.2f %-30.2f %.2f\n",array[i].getNavn(),array[i].getStk(),array[i].getPris(),array[i].getPris() * array[i].getStk(),(array[i].getPris() * array[i].getStk() * 0.85));
             }else {
-                System.out.format("\n%-20s %-23d %-22.2f %-31.2f\n",a6[i].getNavn(),a6[i].getStk(),a6[i].getPris(),a6[i].getPris() * a6[i].getStk());
+                System.out.format("\n%-20s %-23d %-22.2f %-31.2f\n",array[i].getNavn(),array[i].getStk(),array[i].getPris(),array[i].getPris() * array[i].getStk());
             }
         }
 
@@ -155,10 +155,10 @@ public class Main {
         ud.write("Varenavn             Stk                  Stk-pris             Samlet pris          Samlet pris med rabat\n");
 
         for (int i = 0; i < antal; i++) {
-            if (a6[i].getStk() > 10){
-            ud.format("\n%-20s %-23d %-22.2f %-30.2f %.2f\n",a6[i].getNavn(),a6[i].getStk(),a6[i].getPris(),a6[i].getPris() * a6[i].getStk(),(a6[i].getPris() * a6[i].getStk() * 0.85));
+            if (array[i].getStk() > 10){
+            ud.format("\n%-20s %-23d %-22.2f %-30.2f %.2f\n",array[i].getNavn(),array[i].getStk(),array[i].getPris(),array[i].getPris() * array[i].getStk(),(array[i].getPris() * array[i].getStk() * 0.85));
             }else
-            ud.format("\n%-20s %-23d %-22.2f %-31.2f\n",a6[i].getNavn(),a6[i].getStk(),a6[i].getPris(),a6[i].getPris() * a6[i].getStk());
+            ud.format("\n%-20s %-23d %-22.2f %-31.2f\n",array[i].getNavn(),array[i].getStk(),array[i].getPris(),array[i].getPris() * array[i].getStk());
         }
 
         ud.write("\n\n\n_________________________________________________________________________________________________________\n");
